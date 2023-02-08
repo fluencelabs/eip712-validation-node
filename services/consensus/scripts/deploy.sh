e37aeb3be18e2b2c6a21cea15e7995da83754469
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2023-02-07 18:24:34 -0600

    update configs

--- services/consensus/scripts/deploy.sh
+++ services/consensus/scripts/deploy.sh
@@ -5,10 +5,11 @@ set -o errexit -o nounset -o pipefail
 ./scripts/build.sh
 
 (
-  aqua dist deploy \
-       --addr /dns4/kras-03.fluence.dev/tcp/19001/wss/p2p/12D3KooWJd3HaMJ1rpLY1kQvcjRPEvnDwcXrH8mJvk7ypcZXqXGE \
-       --data-path configs/consensus_deploy_cfg.json \
+  aqua remote deploy_service \
+       --addr krasnodar-08 \
+       --config-path configs/consensus_deployment_cfg.json \
        --service consensus-service \
+       --sk cvFkezQeWa1vNUTa+NT6L0c65mHXLfmpz4r2WMjrxZw=
        >> \
         deployed_service_data.txt
 
